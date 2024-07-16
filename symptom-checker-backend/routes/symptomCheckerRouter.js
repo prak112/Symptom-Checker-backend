80d286e519b398fe5335637b47d2ce78f39d6b2a
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-17 00:53:29 +0300

    feat: Add specific and general diagnosis routes

--- symptom-checker-backend/routes/symptomCheckerRouter.js
+++ symptom-checker-backend/routes/symptomCheckerRouter.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const symptomCheckerController = require('../controllers/symptomChecker')
 
-router.post('/symptoms', symptomCheckerController.getDiagnosis)
-
+// 'Specific' search result
+router.post('/specific', symptomCheckerController.getSpecificDiagnosis)
+// 'General' search result
+router.post('/general', symptomCheckerController.getGeneralDiagnosis)
 
 module.exports = router
