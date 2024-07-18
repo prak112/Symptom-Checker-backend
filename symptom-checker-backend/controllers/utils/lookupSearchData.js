dac9fd41940b8fe2eec13ed286e1272d890711f3
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-18 18:49:56 +0300

    debug: Update to use requestHelper module

--- symptom-checker-backend/controllers/utils/lookupSearchData.js
+++ symptom-checker-backend/controllers/utils/lookupSearchData.js
@@ -1,28 +1,28 @@
+const requestHelper = require('./requestBuilder')
+
 async function generateDiagnosisData(requestOptions, searchDataOutput) {
     // initialize diagnosis data
     const browserUrlsArray = [];
     const titlesArray = [];
     const detailsArray = [];
     // lookup each foundationUri from searchDataOutput
-    for (uri of searchDataOutput.foundationURI) {
-        const lookupUrl = 'https://id.who.int/icd/release/11/2024-01/mms/lookup';
-        const lookupParams = {
-            foundationUri: uri
-        };
-        const lookupEndpoint = apiHelper.buildRequestEndpoint(lookupParams, lookupUrl);
-        const lookupResponse = await fetch(lookupEndpoint, requestOptions);
-        const lookupData = await lookupResponse.json();
+    const lookupUrl = 'https://id.who.int/icd/release/11/2024-01/mms/lookup';
+    const lookupParams = {
+        foundationUri: searchDataOutput.foundationURI
+    };
+    const lookupEndpoint = requestHelper.buildRequestEndpoint(lookupParams, lookupUrl);
+    const lookupResponse = await fetch(lookupEndpoint, requestOptions);
+    const lookupData = await lookupResponse.json();
 
-        // extract 'browserUrl','title','definition'
-        browserUrlsArray.push(lookupData.browserUrl);
-        titlesArray.push(lookupData.title["@value"]);
-        detailsArray.push(lookupData.definition["@value"]);
+    // extract 'browserUrl','title','definition'
+    browserUrlsArray.push(lookupData.browserUrl);
+    titlesArray.push(lookupData.title["@value"]);
+    detailsArray.push(lookupData.definition["@value"]);
 
-        console.log(`\nVisit ICD WHO website for more info : ${lookupData.browserUrl}
-            Diagnosed condition : ${lookupData.conditionTitle}
-            General details : ${lookupData.conditionDetails}
-            \n`);
-    }
+    console.log(`\nVisit ICD WHO website for more info : ${lookupData.browserUrl}
+        Diagnosed condition : ${lookupData.title["@value"]}
+        General details : ${lookupData.definition["@value"]}
+        \n`);
 
     // pack searchDataOutput and LookUp API query data variables
     const diagnosisData = {
