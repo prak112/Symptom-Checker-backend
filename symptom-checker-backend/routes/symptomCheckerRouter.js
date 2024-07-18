1b56ac9492947797e981dcbf8c716d4aba772bd7
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-18 18:47:25 +0300

    debug: Update symptomCheckerRouter.js

--- symptom-checker-backend/routes/symptomCheckerRouter.js
+++ symptom-checker-backend/routes/symptomCheckerRouter.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const symptomCheckerController = require('../controllers/symptomChecker')
 
-// 'Specific' search result
-router.post('/specific', symptomCheckerController.getSpecificDiagnosis)
 // 'General' search result
 router.post('/general', symptomCheckerController.getGeneralDiagnosis)
+// 'Specific' search result
+router.post('/specific', symptomCheckerController.getSpecificDiagnosis)
+
 
 module.exports = router
