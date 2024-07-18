0c53c59a61c433e3cb4b91b73190cbccb64ae355
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-18 18:45:29 +0300

    feat: Enable CORS and add ICD API authentication

--- symptom-checker-backend/app.js
+++ symptom-checker-backend/app.js
@@ -15,14 +15,67 @@
 // intial setup
 const express = require('express')
 const app = express()
+const cors = require('cors')  // cross-origin resource sharing
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const apiAuthenticator = require('./utils/apiAuthenticator')
 const symptomCheckerRouter = require('./routes/symptomCheckerRouter')
 
+
+// Setup ICD API access
+// ICD API authentication to OAUTH 2.0 Token Endpoint (token from API for access)
+const tokenEndpoint = 'https://icdaccessmanagement.who.int/connect/token'
+const clientId = config.CLIENT_ID
+const clientSecret = config.CLIENT_SECRET
+logger.info('Credentials loaded')
+// collect request data as 'payload'
+const payload = {
+    'client_id': clientId,
+    'client_secret': clientSecret,
+    'scope': 'icdapi_access',
+    'grant_type': 'client_credentials'
+}
+// convert payload to URL-encoded format
+const requestBody = Object.keys(payload)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(payload[key]))
+    .join('&')
+logger.info('Payload URL-encoded')
+// setup request options for fetching token
+const tokenOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body: requestBody
+}
+logger.info('Options set');
+
+
 // load middleware
-app.use(express.json())
+app.use(cors())   // accepts frontend communication from different origin
+app.use(express.json()) // json-parser for request body
+app.use(middleware.requestLogger)
+// ICD API authentication process
+logger.info('Initiate auth from app.js...')
+const token = apiAuthenticator.authenticateApiAccess(tokenEndpoint, tokenOptions) 
+app.use((request, response, next) => {
+   try {
+      request.accessToken = token
+      next()
+   } 
+   catch (error) {
+      logger.error('ICD API Authentication error : ', error)
+      response.status(500).send('ICD API Authentication Failed')
+   }
+})
 
 // route requests
-app.use(apiAuthenticator.authenticateApiAccess)
 app.use('/api/protected/symptoms', symptomCheckerRouter)
+
+// error handlers
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 
 module.exports = app
