2957fcf149a34de6b1ae66ae60288b513bf9a6eb
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-18 18:45:03 +0300

    refactor: update token renewal, add required tokenInfo

--- symptom-checker-backend/utils/apiAuthenticator.js
+++ symptom-checker-backend/utils/apiAuthenticator.js
@@ -1,57 +1,27 @@
-// imports
-const config = require('./config')
-
-// Setup ICD API access
-// ICD API authentication to OAUTH 2.0 Token Endpoint (token from API for access)
-const tokenEndpoint = 'https://icdaccessmanagement.who.int/connect/token'
-const clientId = config.CLIENT_ID
-const clientSecret = config.CLIENT_SECRET
-console.log('Credentials loaded')
-
-
-// collect payload data as 'payload'
-const payload = {
-    'client_id': clientId,
-    'client_secret': clientSecret,
-    'scope': 'icdapi_access',
-    'grant_type': 'client_credentials'
-}
-
-// convert payload to URL-encoded format
-const requestBody = Object.keys(payload)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(payload[key]))
-    .join('&')
-console.log('Payload URL-encoded')
-
-// setup request options for fetching token
-const tokenOptions = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    body: requestBody
-}
-console.log('Options set');
-
-
 // authentication request to ICD API with scheduled renewal of token
 let tokenInfo = {
+    tokenEndpoint: null,
+    tokenOptions: null,
     accessToken: null,
     expiresIn: null,
     updateTimeout: null
 }
+console.log('Token info set')
 
 async function authenticateApiAccess(tokenEndpoint, tokenOptions){
 try {
         if (!tokenInfo.accessToken) {
-            const tokenClient = await fetch(tokenEndpoint, tokenOptions)
+            tokenInfo.tokenEndpoint = tokenEndpoint
+            tokenInfo.tokenOptions = tokenOptions
+            const tokenClient = await fetch(tokenInfo.tokenEndpoint, tokenInfo.tokenOptions)
             const data = await tokenClient.json()
             return fetchToken(data)
         } else {
             return tokenInfo.accessToken
         }
     } catch (error) {
-        throw new Error('ERROR fetching Token : ', error.message)
+        console.log('ERROR fetching Token : ', error.message)
+        next(error)
     }
 }
 
@@ -64,20 +34,26 @@ function fetchToken(tokenData) {
         // schedule token update
         // expires_in data available, reset updateTimeout callback
         if(tokenInfo.updateTimeout){
-            clearTimeout(tokenInfo.updateTimeout)
+            clearInterval(tokenInfo.updateTimeout)
             console.log('Clear Token update callback')
         }
         // schedule api authentication 30 seconds before
-        let localTime = new Date().toLocaleTimeString()
-        tokenInfo.updateTimeout = setTimeout(async() => {
-            await authenticateApiAccess(tokenEndpoint, tokenOptions);
-            console.log('Token updated - ', localTime);
+        let currentTime = new Date() // time in UTC format
+        let tokenGeneratedTime = currentTime.toLocaleTimeString()
+        let tokenExpiryTime = new Date(
+            currentTime.getTime() + (tokenInfo.expiresIn * 1000)
+        ).toLocaleTimeString()
+        tokenInfo.updateTimeout = setInterval(async() => {
+            await authenticateApiAccess(tokenInfo.tokenEndpoint, tokenInfo.tokenOptions);
+            let tokenRenewedTime = new Date().toLocaleTimeString()
+            console.log('Token updated - ', tokenRenewedTime);
         }, (tokenInfo.expiresIn - 30) * 1000);
 
-        console.log(`Token generated at ${localTime}\nToken will be renewed in ${tokenInfo.expiresIn - 30} seconds`)
+        console.log(`Token generated at ${tokenGeneratedTime}\nToken will be renewed at ${tokenExpiryTime}`)
         return tokenInfo.accessToken
     } catch (error) {
-        throw new Error('ERROR fetching Token : ', error.message)
+        console.log('ERROR fetching Token : ', error.message)
+        next(error)
     }
 }
 
