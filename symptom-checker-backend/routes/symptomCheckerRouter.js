b3da9105f136a6e5496d58dba084682518cc86c0
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-01 18:53:49 +0300

    refactor: Update controller import path

--- symptom-checker-backend/routes/symptomCheckerRouter.js
+++ symptom-checker-backend/routes/symptomCheckerRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const symptomCheckerController = require('../controllers/symptomChecker')
+const symptomCheckerController = require('../controllers/symptomChecker/symptomChecker')
 
 // 'General' search result
 router.post('/general', symptomCheckerController.getGeneralDiagnosis)
