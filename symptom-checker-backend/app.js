a6772df000188b9f2b94c2c217d2a67d6bf986b6
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-01 17:08:51 +0300

    feat: Refactor app.js for improved middleware organization

--- symptom-checker-backend/app.js
+++ symptom-checker-backend/app.js
@@ -19,63 +19,45 @@ const cors = require('cors')  // cross-origin resource sharing
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const apiAuthenticator = require('./utils/apiAuthenticator')
+const apiAuthController = require('./controllers/apiAuthController')
 const symptomCheckerRouter = require('./routes/symptomCheckerRouter')
+const signupRouter = require('./controllers/signup')
 
 
-// Setup ICD API access
-// ICD API authentication to OAUTH 2.0 Token Endpoint (token from API for access)
-const tokenEndpoint = 'https://icdaccessmanagement.who.int/connect/token'
-const clientId = config.CLIENT_ID
-const clientSecret = config.CLIENT_SECRET
-logger.info('Credentials loaded')
-// collect request data as 'payload'
-const payload = {
-    'client_id': clientId,
-    'client_secret': clientSecret,
-    'scope': 'icdapi_access',
-    'grant_type': 'client_credentials'
-}
-// convert payload to URL-encoded format
-const requestBody = Object.keys(payload)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(payload[key]))
-    .join('&')
-logger.info('Payload URL-encoded')
-// setup request options for fetching token
-const tokenOptions = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    body: requestBody
-}
-logger.info('Options set');
+// Setup MongoDB connection
+const mongoose = require('mongoose')
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.info('Error connecting to MongoDB!\nERROR: ', error.message)
+    })
 
 
-// load middleware
-app.use(cors())   // accepts frontend communication from different origin
-app.use(express.json()) // json-parser for request body
+// Load Middleware
+
+// ICD API authentication middeware
+app.use(apiAuthController.authenticate)
+
+// CORS-accepts frontend communication from different origin
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
+// json-parser for request body   
+app.use(express.json()) 
 app.use(middleware.requestLogger)
-// ICD API authentication process
-logger.info('Initiate auth from app.js...')
-const token = apiAuthenticator.authenticateApiAccess(tokenEndpoint, tokenOptions) 
-app.use((request, response, next) => {
-   try {
-      request.accessToken = token
-      next()
-   } 
-   catch (error) {
-      logger.error('ICD API Authentication error : ', error)
-      response.status(500).send('ICD API Authentication Failed')
-   }
-})
 
-// route requests
+
+// Route requests
+app.use('/public/signup', signupRouter)
 app.use('/api/protected/symptoms', symptomCheckerRouter)
 
-// error handlers
+// Error handlers
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
-
+// export to index
 module.exports = app
