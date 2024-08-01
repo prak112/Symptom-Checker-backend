16558a1d7c7af48787807451fc2794b7e70061b8
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-01 18:54:52 +0300

    refactor: Add tokenExtractor and userExtractor middleware

--- symptom-checker-backend/utils/middleware.js
+++ symptom-checker-backend/utils/middleware.js
@@ -1,4 +1,7 @@
+const { response } = require('../app')
 const logger = require('./logger')
+const config = require('./config')
+const User = require('../models/user')
 
 // load Middleware (VERY PARTICULAR ORDER)
 const morgan = require('morgan')    // request logger
@@ -7,6 +10,36 @@ const requestLogger = morgan('dev')
 // Request Logger ONLY for development purposes - POST method
 // morgan.token('body', (req, res) => JSON.stringify(req.body) );
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+
+// assign JWT from request headers to request parameters
+const tokenExtractor = (request, response, next) => {
+    request.token = request.cookies['auth_token']
+    next()
+}
+
+// validate user's JWT to prevent overlap/misinterpretation of information input by user 
+// used to validate requests
+const userExtractor = async(request, response, next) => {
+    try{
+        const decodedToken = jwt.verify(request.token, config.SECRET)
+        const user = await User.findById(decodedToken.id)
+        if(!user){
+            return response
+                .status(401)
+                .json({error: 'User not found. Authentication Failed.'})
+        }
+        request.user = user
+        next()
+    }
+    catch(error){
+        logger.error(error)
+        response
+            .status(401)
+            .json({error: 'Invalid Token. User authentication failed.' })
+    }
+}
+
 
 // load before-last Middleware
 const unknownEndpoint = (request, response) => {
@@ -32,4 +65,4 @@ const errorHandler = (error, request, response, next) => {
 }
 
 // export to app
-module.exports = { requestLogger, unknownEndpoint, errorHandler }
+module.exports = { requestLogger, tokenExtractor, userExtractor, unknownEndpoint, errorHandler }
