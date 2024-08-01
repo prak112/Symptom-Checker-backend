16e0d283b0ef3dc7019396783df92e3d779b1d29
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-01 18:54:38 +0300

    chore: Add SECRET to config.js

--- symptom-checker-backend/utils/config.js
+++ symptom-checker-backend/utils/config.js
@@ -2,5 +2,6 @@ const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const SECRET = process.env.SECRET
 
-module.exports = { CLIENT_ID, CLIENT_SECRET, PORT, MONGODB_URI }
+module.exports = { CLIENT_ID, CLIENT_SECRET, PORT, MONGODB_URI, SECRET }
