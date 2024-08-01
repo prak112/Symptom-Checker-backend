44b47170e33e0cf18ea9f5510ce88b263d7ba3ce
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-01 17:08:20 +0300

    chore: Add MONGODB_URI

--- symptom-checker-backend/utils/config.js
+++ symptom-checker-backend/utils/config.js
@@ -1,5 +1,6 @@
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
-module.exports = { CLIENT_ID, CLIENT_SECRET, PORT }
+module.exports = { CLIENT_ID, CLIENT_SECRET, PORT, MONGODB_URI }
