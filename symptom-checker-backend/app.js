74b0b81360c7a6b1d0a01e121010ba1b0480bc5c
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-05 18:54:13 +0300

    chore: Remove non-essential comments

--- symptom-checker-backend/app.js
+++ symptom-checker-backend/app.js
@@ -1,17 +1,3 @@
-/**
- * MAIN OBJECTIVE: Refactor code to setup Express server
- ** 
- * GOAL #1: Authenticate ICD API through middleware
- * GOAL #2: Setup requests with symptoms
-    * use 'searchText' in API Call -
-    * /icd/release/11/2024-01/mms/autocode?searchText=sore%20throat&matchThreshold=0.75
- * GOAL #3: Process and generate data to be placed for triage-level assessment in React Component (Diagnosis.jsx)
-    * Use 'foundationURI' in API Call -
-    * /icd/release/11/2024-01/mms/lookup?foundationUri=http%3A%2F%2Fid.who.int%2Ficd%2Fentity%2F633724732
-    * response includes 'browserUrl', 'title', 'definition', 
-    * other possible conditions list - 'exclusion', 'indexTerm'
-**/
-
 // Intial setup
 const config = require('./utils/config')
 const express = require('express')
