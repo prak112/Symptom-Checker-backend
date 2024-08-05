bbecffcc8a69e58c86c39404cb10bfbfcae38534
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-05 19:15:39 +0300

    feat: Add login and signup routes and controllers

---
+++ symptom-checker-backend/models/symptom.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const symptomSchema = new mongoose.Schema({
+    symptom: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+// transform DB output fields for relevant data
+symptomSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+// export to controllers/symptomChecker
+module.exports = mongoose.model('Symptom', symptomSchema )
