4cdf8de6f3fa56151c94daab397bb0d44e68b575
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-05 18:56:07 +0300

    feat: Add secret variables for Database

--- symptom-checker-backend/utils/config.js
+++ symptom-checker-backend/utils/config.js
@@ -2,6 +2,16 @@ const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
-const SECRET = process.env.SECRET
+const USER_SECRET = process.env.USER_SECRET
+const DB_SECRET = process.env.DB_SECRET
+const OLD_DB_SECRET = process.env.OLD_DB_SECRET
 
-module.exports = { CLIENT_ID, CLIENT_SECRET, PORT, MONGODB_URI, SECRET }
+module.exports = { 
+    CLIENT_ID, 
+    CLIENT_SECRET, 
+    PORT, 
+    MONGODB_URI, 
+    USER_SECRET,
+    DB_SECRET,
+    OLD_DB_SECRET
+ }
