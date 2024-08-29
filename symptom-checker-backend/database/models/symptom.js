783e437335adcb7a0e07efa32d2b7a8de163ea2d
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-29 22:23:21 +0300

    chore: Add comments and remove commented code

--- symptom-checker-backend/database/models/symptom.js
+++ symptom-checker-backend/database/models/symptom.js
@@ -1,87 +1,7 @@
-// const mongoose = require('mongoose')
-
-// // Sub-schemas for diagnosis
-// const resultSchema = new mongoose.Schema({
-//     label: {
-//         type: [String],
-//     },
-//     score: {
-//         type: [Number],
-//     },
-//     url: {
-//         type: [String]
-//     }
-// })
-// const topResultSchema = new mongoose.Schema({
-//     label: {
-//         type: String,
-//         required: true,
-//     },
-//     score: {
-//         type: Number,
-//         required: true,
-//     },
-//     title: {
-//         type: String,
-//         required: true,
-//     },
-//     detail: {
-//         type: String,
-//         required: true,
-//     },
-//     url: {
-//         type: String,
-//         required: true,
-//     }
-// })
-
-// // Main schema - diagnosis
-// const diagnosisSchema = new mongoose.Schema({
-//     diagnosisData : {
-//         symptoms: {
-//             type: String,
-//             required: true
-//         },
-//         topResult: {
-//             type: topResultSchema,
-//             required: true
-//         },
-//         includedResults: {
-//             type: resultSchema,
-//         },
-//         excludedResults: {
-//             type: resultSchema,
-//         }
-//     }   
-// })
-
-// // Symptom collection schema
-// const symptomSchema = new mongoose.Schema({
-//     diagnosis: {
-//         type: [diagnosisSchema],
-//         required: true
-//     },
-//     user:{
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User'
-//     }
-// })
-
-// // transform DB output fields for relevant data
-// symptomSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         returnedObject.id = returnedObject._id.toString()
-//         delete returnedObject._id
-//         delete returnedObject.__v
-//     }
-// })
-
-// // export to controllers/symptomChecker
-// module.exports = mongoose.model('Symptom', symptomSchema )
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Sub-Schemas
 const resultSchema = new Schema({
     label: [String],
     score: [Number],
@@ -103,6 +23,7 @@ const diagnosisDataSchema = new Schema({
     excludedResults: { type: resultSchema, required: true }
 });
 
+// Main schema
 const diagnosisArraySchema = new Schema({
     diagnosis: { type: [diagnosisDataSchema], required: true },
     user:{
