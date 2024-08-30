17d40f1b4f9ee9ac215f1589862c9585f4c52564
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-30 20:53:00 +0300

    feat: Add Controller to create guest user

---
+++ symptom-checker-backend/controllers/auth/user/guest.js
@@ -0,0 +1,38 @@
+// security context libraries
+const crypto = require('crypto')
+const bcrypt = require('bcryptjs')
+// utils
+const logger = require('../../../utils/logger')
+const userAuthenticator = require('../../../utils/userAuth')
+// database schema
+const User = require('../../../database/models/user')
+
+
+exports.createGuestUser = async(request, response) => {
+    try {
+        // register guest user
+        const guestId = crypto.randomUUID().toString()
+        const randomPassword = crypto.randomBytes(32).toString('base64')
+        const saltRounds = 10
+        const guestPasswordHash = await bcrypt.hash(randomPassword, saltRounds)
+        const guestUsername = `guest-${guestId}`
+        const guestUser = new User({
+            username: guestUsername,
+            passwordHash: guestPasswordHash
+        })
+        const savedGuest = await guestUser.save()
+        
+        // Generate JWT and set response cookie
+        const guestToken = userAuthenticator.generateAuthToken(savedGuest)
+        userAuthenticator.setAuthToken(response, guestToken)
+
+        return response
+            .status(200)
+            .send({ username: savedGuest.username })
+    } catch (error) {
+        logger.error('ERROR during Guest User creation : ', error)
+        return response
+            .status(500)
+            .json({ error: 'Internal Server Error' })
+    }
+}
