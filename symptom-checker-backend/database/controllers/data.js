a7b5bdbb4e8ebb4abfc4e4180dc7e7f763b51acc
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-30 21:08:00 +0300

    refactor: Handle data storage only

--- symptom-checker-backend/database/controllers/data.js
+++ symptom-checker-backend/database/controllers/data.js
@@ -1,71 +1,33 @@
 // schemas
 const User = require('../models/user')
 const Symptom = require('../models/symptom')
-// security context libraries
-const crypto = require('crypto')
-const bcrypt = require('bcryptjs')
 
 /** TO DO - 
- * add createGuestUser in middleware.js
-    * preserve guest user UUID in request.user
+ * DONE - create /guest route and controller
+ * DONE - use authentication middleware for login handling
+ * DONE - refactor manageDatabase
     * resolves repetition of Guest User creation for every request from same session
- * DONE - Update Database schema in workflow (Thanks to Pieces! GitHub Copilot sucks!)
-**/
+ * DONE - Update Database schema in workflow 
+    * (Thanks to Pieces! GitHub Copilot sucks!)
+ * DONE - DEBUG - Storage issue with user.id in Symptom object
+ */
 
-exports.manageDatabase = async(request, diagnosisDataArray) => {
+exports.manageDatabase = async(user, diagnosisDataArray) => {
 try {
-        // find user
-        const foundUser = await User.findOne({ username: request.user }) ? true : false
-    
-        if(!foundUser) {
-            console.log('Creating Guest User...')
+        // get user
+        console.log('User Information BEFORE : ', user)
+        // update Symptom collection with user object id and symptom data
+        const userDiagnosis = new Symptom({
+            diagnosis: diagnosisDataArray,
+            user: user.id
+        })
+        const savedDiagnosis = await userDiagnosis.save()
+        console.log('User Diagnosis : ', savedDiagnosis)
 
-            // create guest user with similar security practices as registered user
-            const guestId = crypto.randomUUID().toString()
-            const randomPassword = crypto.randomBytes(32).toString('base64')
-            const saltRounds = 10
-            const guestPasswordHash = await bcrypt.hash(randomPassword, saltRounds)
-            const guestUsername = `guest-${guestId}`
-            const guestUser = new User({
-                username: guestUsername,
-                passwordHash: guestPasswordHash
-            })
-            const savedGuest = await guestUser.save()
-            // save the guest as current user in server
-            request.user = savedGuest.username
-            console.log('Created Guest User and added to request.user...')
-
-            // update Symptom collection with user object id and symptom data
-            const guestSymptoms = new Symptom({
-                diagnosis: diagnosisDataArray,
-                user: savedGuest._id
-            })
-            const savedSymptoms = await guestSymptoms.save()
-            console.log('Saved Guest Symptoms to DB...')
-
-
-            // update User collection with symptom object id
-            savedGuest.diagnosis = savedGuest.diagnosis.concat(savedSymptoms._id)
-            await savedGuest.save()
-            console.log('GUEST User Information : ', savedGuest)
-            console.log('GUEST User Symptoms : ', guestSymptoms)
-            
-        } else {
-            // get user if found
-            const existingUser = await User.findOne({ username: request.user })
-            // update Symptom collection with user object id and symptom data
-            const userSymptoms = new Symptom({
-                diagnosis: diagnosisDataArray,
-                user: existingUser._id
-            })
-            const savedSymptoms = await userSymptoms.save()
-            console.log('EXISTING User Symptoms : ', savedSymptoms)
-    
-            // update User collection with symptom object id
-            existingUser.symptom = existingUser.symptom.concat(savedSymptoms._id)
-            await existingUser.save()
-            console.log('EXISTING User Information : ', existingUser)
-        }
+        // update User collection with symptom object id
+        user.diagnosis = user.diagnosis.concat(savedDiagnosis._id)
+        await user.save()
+        console.log('User Information AFTER : ', user)
 
         return true
     } catch (error) {
