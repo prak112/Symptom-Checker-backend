7c6c51fb5a764e4008865c5690c375ff38144268
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-30 20:53:37 +0300

    feat: Add Route to guest setup Controller

--- symptom-checker-backend/routes/userAuthRouter.js
+++ symptom-checker-backend/routes/userAuthRouter.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 const signUpController = require('../controllers/auth/user/signup')
 const loginController = require('../controllers/auth/user/login')
+const guestController = require('../controllers/auth/user/guest')
 
 // Signup route
 router.post('/signup', signUpController.registerUser)
 
 // Login route
 router.post('/login', loginController.authenticateUser)
+
+// Guest setup route
+router.post('/guest', guestController.createGuestUser)
 
 module.exports = router
