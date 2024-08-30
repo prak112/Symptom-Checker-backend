2c73586ee046cf2ce8114bff4d5f879074c1e565
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-30 21:08:17 +0300

    feat: Add userExtractor middleware

--- symptom-checker-backend/routes/symptomCheckerRouter.js
+++ symptom-checker-backend/routes/symptomCheckerRouter.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const middleware = require('../utils/middleware')
 const symptomCheckerController = require('../controllers/symptomChecker/symptomChecker')
 
 // 'General' search result
-router.post('/general', symptomCheckerController.getGeneralDiagnosis)
+router.post('/general', middleware.userExtractor, symptomCheckerController.getGeneralDiagnosis)
 // 'Specific' search result
-router.post('/specific', symptomCheckerController.getSpecificDiagnosis)
+router.post('/specific', middleware.userExtractor, symptomCheckerController.getSpecificDiagnosis)
 
 
 module.exports = router
