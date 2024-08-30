41c9e2830d415a8138dcb08848e2fcfc2b59b83b
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-30 21:01:06 +0300

    feat: Refactor to add authentication utility

--- symptom-checker-backend/controllers/auth/user/login.js
+++ symptom-checker-backend/controllers/auth/user/login.js
@@ -1,14 +1,18 @@
-const jwt = require('jsonwebtoken')
+// security context libraries
 const bcrypt = require('bcryptjs')
-const config = require('../../../utils/config')
+// utilities
+const logger = require('../../../utils/logger')
+const authHandler = require('../../../utils/userAuth')
+// database scehema
 const User = require('../../../database/models/user')
+
 
 exports.authenticateUser = async(request, response) => {
     try {
         const { username, password } = request.body
         const registeredUser = await User.findOne({username: username})
         if(!registeredUser){
-            console.error('User does not exist. Signup for access.')
+            logger.error('User does not exist. Signup for access.')
             return response
                 .status(401)
                 .json({ error: 'User does not exist. Signup for access.' })
@@ -19,34 +23,17 @@ exports.authenticateUser = async(request, response) => {
                 .status(401)
                 .json({ error: 'Invalid Credentials. Check and enter again.' })
         }
-    
+
         // if valid username and password
-        const userToAuthenticate = {
-            username: registeredUser.username,
-            id: registeredUser.id
-        }
-        // generate 2 hours valid JWT 
-        const token = jwt.sign(
-            userToAuthenticate,
-            config.USER_SECRET,
-            {
-                expiresIn: 2 * 60 * 60
-            }
-        )
-    
+        const token = authHandler.generateAuthToken(registeredUser)    
         // wrap JWT in httpCookie to be passed in request.headers
-        response.cookie('auth_token', token, {
-            httpOnly: true,
-            secure: false, // true if https
-            maxAge: 2 * 60 * 60, // match token expiresIn
-            sameSite: 'strict'  // mitigate CSRF attacks
-        })
-        response
+        authHandler.setAuthToken(response, token)
+
+        return response
             .status(200)
             .send({username: registeredUser.username})
-    } 
-    catch (error){
-        console.error('Error during Login: ', error)
+    } catch (error) {
+        logger.error('Error during Login: ', error)
         return response
             .status(500)
             .json({ error: 'Internal Server Error.' })
