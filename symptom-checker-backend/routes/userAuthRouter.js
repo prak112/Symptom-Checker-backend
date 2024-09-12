777a1d03ed09999446b382bdb58306ac2b7ea68f
PrakGov <55034062+prak112@users.noreply.github.com>
2024-09-12 18:20:35 +0300

    feat: Add logout functionality with cleanup

--- symptom-checker-backend/routes/userAuthRouter.js
+++ symptom-checker-backend/routes/userAuthRouter.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
+const guestController = require('../controllers/auth/user/guest')
 const signUpController = require('../controllers/auth/user/signup')
 const loginController = require('../controllers/auth/user/login')
-const guestController = require('../controllers/auth/user/guest')
+const logoutController = require('../controllers/auth/user/logout')
+
+// Guest setup route
+router.post('/guest', guestController.createGuestUser)
 
 // Signup route
 router.post('/signup', signUpController.registerUser)
@@ -9,7 +13,8 @@ router.post('/signup', signUpController.registerUser)
 // Login route
 router.post('/login', loginController.authenticateUser)
 
-// Guest setup route
-router.post('/guest', guestController.createGuestUser)
+// Logout route - To Be Implemented
+router.post('/logout', logoutController.clearUserSession)
+
 
 module.exports = router
