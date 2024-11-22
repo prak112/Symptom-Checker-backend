3e09839734b703145b54590a18ba27b4f42a9185
PrakGov <55034062+prak112@users.noreply.github.com>
2024-11-22 21:10:53 +0200

    docs: Add comments about authentication middleware usage

--- symptom-checker-backend/routes/symptomCheckerRouter.js
+++ symptom-checker-backend/routes/symptomCheckerRouter.js
@@ -3,9 +3,13 @@ const router = express.Router()
 const middleware = require('../utils/middleware')
 const symptomCheckerController = require('../controllers/business/symptomChecker')
 
-// 'General' search result
+/**
+ * Authentication middleware verifies the user by the JWT in the Cookie header and approves or denies the service request
+ */
+// 'General' search
 router.post('/general', middleware.userExtractor, symptomCheckerController.getGeneralDiagnosis)
-// 'Specific' search result
+
+// 'Specific' search
 router.post('/specific', middleware.userExtractor, symptomCheckerController.getSpecificDiagnosis)
 
 // export to app
