b3e71f2ca5148f06ca3254035449c79af522eb1a
PrakGov <55034062+prak112@users.noreply.github.com>
2024-12-09 18:44:03 +0200

    feat: Add endpoint to retrieve diagnosis data

--- symptom-checker-backend/database/controllers/adminData.js
+++ symptom-checker-backend/database/controllers/adminData.js
@@ -12,8 +12,42 @@ exports.getAllUsers = async(request, response, next) => {
     
     return response.json(users)
   } catch (error) {
-    console.error('ERROR: You do not have access to Admin services.');
+    console.error(`ERROR fetching users from database.\nERROR: ${error}`);
     throw error;
     // next();
+  }
+}
+
+exports.getAllDiagnosis = async(request, response, next) => {
+  try {
+    const diagnosis = await Symptom
+      .find({})
+      .populate({
+        path: 'diagnosis',
+        model: 'Diagnosis', // reference in schema
+          select: {
+            symptom: 1,
+            analysis: 1,
+            diagnosedAt: 1,
+            topResult: 1,
+            includedResults: 0,
+            excludedResults: 0,
+            _id: 0,
+            diagnosedAt: 1,
+          }
+        }
+      );
+    
+    // const diagnosis = await Symptom
+    //   .find({},
+    //     {
+
+    //     }
+    //   )
+
+    return response.json(diagnosis)
+  } catch (error) {
+    console.error(`ERROR fetching diagnosis from database.\nERROR: ${error}`);
+    throw error;
   }
 }
--- symptom-checker-backend/routes/adminDataRouter.js
+++ symptom-checker-backend/routes/adminDataRouter.js
@@ -6,5 +6,8 @@ const adminDataController = require('../database/controllers/adminData')
 // GET all users
 router.get('/users', middleware.userExtractor, adminDataController.getAllUsers)
 
+// GET all diagnosis
+router.get('/diagnosis', middleware.userExtractor, adminDataController.getAllDiagnosis)
+
 // export to app
 module.exports = router
