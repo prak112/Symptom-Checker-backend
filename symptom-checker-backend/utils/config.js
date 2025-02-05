19422f6dd92d1009c9d7de13de94c8b0b83f9c87
PrakGov <55034062+prak112@users.noreply.github.com>
2025-02-05 19:51:30 +0200

    chore: Remove secret variables from config

--- symptom-checker-backend/utils/config.js
+++ symptom-checker-backend/utils/config.js
@@ -2,9 +2,6 @@ const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
-const USER_SECRET = process.env.USER_SECRET
-const DB_SECRET = process.env.DB_SECRET
-const OLD_DB_SECRET = process.env.OLD_DB_SECRET
 const WEB_FRONTEND = process.env.WEB_FRONTEND
 const MOBILE_FRONTEND = process.env.MOBILE_FRONTEND
 const ADMIN_FRONTEND = process.env.ADMIN_FRONTEND
@@ -14,9 +11,6 @@ module.exports = {
     CLIENT_SECRET, 
     PORT, 
     MONGODB_URI, 
-    USER_SECRET,
-    DB_SECRET,
-    OLD_DB_SECRET,
     WEB_FRONTEND,
     MOBILE_FRONTEND,
     ADMIN_FRONTEND,
